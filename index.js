810956cac7b577db5073c3e04a9c0978db164092
THEALAN <105835098+Zack-Dx@users.noreply.github.com>
2022-10-14 12:19:13 +0530

    Fixed loop issue.

--- index.js
+++ index.js
@@ -9,21 +9,13 @@ const nameimplement = () => {
   coderun.innerHTML = `${user}@${user}-VirtualBox:~$`;
 };
 
-let user = "";
-while (user.length < 1) {
-  user = prompt("Enter your name?");
-  if (user == "") {
-    alert("Enter your name properly!");
+let user = null;
+while (user == null || user == "") {
+  user = prompt("Enter your name to login?");
+  if (user == null || user == "") {
+    alert("No access without Username!");
   } else {
+    alert("login success!");
     nameimplement();
-    alert("Admin identified.");
   }
 }
-
-let parafetch =document.getElementById ("para-login")
-const login = setTimeout(() => {
-parafetch.innerHTML = "Login Success!"
-}, 2000);
-
-
-login()
